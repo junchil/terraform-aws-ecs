be850108ca6dd9c3780e79900272ed899bbaddaa
Steve Liu <ljc1993917@gmail.com>
2021-06-21 15:55:24 +1000

    terraform fmt

--- cluster/golang.tf
+++ cluster/golang.tf
@@ -17,7 +17,7 @@ resource "aws_ecs_service" "golang-service" {
   }
 
   network_configuration {
-    subnets = var.subnet_ids
+    subnets         = var.subnet_ids
     security_groups = [aws_security_group.golang_sg.id]
   }
 }
